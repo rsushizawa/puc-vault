74163932f5580dea3f6fa07fd260e9ad27ae8e4b
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2026-04-04 17:19:14 -0300

    feat: community-hero component + test working

--- src/apps/web-app/src/app/page.tsx
+++ src/apps/web-app/src/app/page.tsx
@@ -1,10 +1,19 @@
 import NavBar from "@/components/navbar";
-
+import CommunityHero from "@/components/community-hero";
 export default function Home() {
+  const defaultProps = {
+    communityName: "Engenharia da Computação",
+    memberCount: "12.4k",
+    repositoryType: "PUC repositotyType",
+    bannerSrc: "/banner.jpg",
+    iconSrc: "/icon.png",
+  };
   return (
     <div className="bg-surface-base min-h-screen flex flex-col">
       <NavBar />
-      <main></main>
+      <main>
+        <CommunityHero {...defaultProps}></CommunityHero>
+      </main>
     </div>
   );
 }
